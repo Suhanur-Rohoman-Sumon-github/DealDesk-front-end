2032f9166ea92f3fb74d160dcbee761aa503f1cc
Suhnaur-Rohoman-Sumon-github <sumonsuhanurrohoman@gmail.com>
2025-04-21 17:18:36 +0600

    Fix: add alias resolution in next.config.js for Vercel build

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,23 @@
-module.exports = {
+// next.config.ts
+import path from 'path';
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "**",
+        protocol: 'https',
+        hostname: '**',
       },
     ],
   },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      '@': path.resolve(__dirname, 'src'),
+    };
+    return config;
+  },
 };
+
+export default nextConfig;

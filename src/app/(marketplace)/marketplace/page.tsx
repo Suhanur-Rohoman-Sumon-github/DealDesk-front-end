5b434e4349c92ac6c984a9e757990cf9ee8c9b41
Suhnaur-Rohoman-Sumon-github <sumonsuhanurrohoman@gmail.com>
2025-04-21 16:50:44 +0600

    serversite problem solved

--- src/app/(marketplace)/marketplace/page.tsx
+++ src/app/(marketplace)/marketplace/page.tsx
@@ -1,10 +1,18 @@
-import Products from '@/components/marketplace/Products';
+
+import dynamic from 'next/dynamic';
 import React from 'react';
+
+const MarketplaceComponent = dynamic(
+  () => import("@/components/marketplace/Products"),
+  {
+    ssr: false,
+  }
+);
 
 const page = () => {
   return (
     <div className="p-4  pt-6 flex flex-col gap-4">
-      <Products />
+      <MarketplaceComponent />
     </div>
   );
 };

18a5367879e0afc88efe8152406ca7aac36c0006
Suhnaur-Rohoman-Sumon-github <sumonsuhanurrohoman@gmail.com>
2025-05-02 18:07:04 +0600

    twak to changed

--- src/context/GoogleAnaliticsProvider.tsx
+++ src/context/GoogleAnaliticsProvider.tsx
@@ -10,16 +10,15 @@ const GoogleAnalyticsProvider = ({ children }: Props) => {
     <>
       {/* Google Analytics */}
       <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=G-9NZLRYV3XV`}
+        src="https://www.googletagmanager.com/gtag/js?id=G-9NZLRYV3XV"
         strategy="afterInteractive"
       />
       <Script id="google-analytics" strategy="afterInteractive">
         {`
-          wwindow.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-9NZLRYV3XV');
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-9NZLRYV3XV');
         `}
       </Script>
       {children}

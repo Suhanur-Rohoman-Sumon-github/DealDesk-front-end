41a386dddfad8cf1a34dd65515f4ddac46dcd2ad
Suhnaur-Rohoman-Sumon-github <sumonsuhanurrohoman@gmail.com>
2025-05-02 17:46:44 +0600

    twak to changed

--- src/app/(marketplace)/layout.tsx
+++ src/app/(marketplace)/layout.tsx
@@ -41,23 +41,26 @@ export default function RootLayout({
             </div>
           </div>
 
-          <Script
-            id="tawk-to"
-            strategy="afterInteractive"
-            dangerouslySetInnerHTML={{
-              __html: `
-      var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-      (function(){
-        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-        s1.async=true;
-        s1.src='https://embed.tawk.to/65cb25d40ff6374032cc8f5e/1hmgonj3q';
-        s1.charset='UTF-8';
-        s1.setAttribute('crossorigin','*');
-        s0.parentNode.insertBefore(s1,s0);
+       
+<Script
+  id="tawk-to-script"
+  type="text/javascript"
+  dangerouslySetInnerHTML={{
+    __html: `
+      var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+      (function () {
+        var s1 = document.createElement("script"),
+          s0 = document.getElementsByTagName("script")[0];
+        s1.async = true;
+        s1.src = "https://embed.tawk.to/6812c4cbcba56419020c08d8/1iq4l1f2o";
+        s1.charset = "UTF-8";
+        s1.setAttribute("crossorigin", "*");
+        s0.parentNode.insertBefore(s1, s0);
       })();
     `,
-            }}
-          />
+  }}
+/>
+
         </Providers>
       </body>
     </html>
